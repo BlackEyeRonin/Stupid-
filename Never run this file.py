7e830ccd1a991565ec55486db1adadc613d40144
SkyLine <155222029+BlackEyeRonin@users.noreply.github.com>
2024-05-16 19:33:56 +0530

    Create Never run this file.py

---
+++ Never run this file.py
@@ -0,0 +1,8 @@
+#Infinite loop
+
+food = input('Enter your food name:') #Define what the constant is
+while food == "":                            #Write the first condition
+       print ("Please enter you food name!") #1st condition's output
+       #No repeating input
+
+print(f"Nice that your fav food is {food}") #Final output 
